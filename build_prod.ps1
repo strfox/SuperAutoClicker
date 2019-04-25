906f70f420203da3d0f5b9ff17ac4205aeb1f0c4
Michel Faria <michelfariarj@gmail.com>
2019-04-25 11:10:39 -0300

    Build script

--- build_prod.ps1
+++ build_prod.ps1
@@ -1,4 +1,27 @@
-mkdir Release
-cd Release
-cmake -DCMAKE_BUILD_TYPE=Release ..
-mingw32-make
+param(
+    [bool]$Debug,
+    [bool]$Clean,
+    [string[]]$Passthrough
+)
+
+$ErrorActionPreference = "Stop"
+
+$target = If ($debug) { "Debug" } else { "Release" }
+$dir = ".\$target\"
+
+if ($clean) {
+    Write-Host "Cleaning $dir..."
+    Get-ChildItem -Path $dir -Include *.* -File -Recurse | foreach { $_.Delete() }
+}
+
+New-Item -ItemType Directory -Force -Path $dir | Out-Null
+Set-Location -Path $dir
+
+try {
+cmake.exe -G 'MinGW Makefiles' -DCMAKE_BUILD_TYPE=$target .. 
+mingw32-make.exe @Passthrough
+} catch [System.Management.Automation.RemoteException] {
+  Write-Host 'Could not build: '$_.Exception.Message
+} finally {
+  Set-Location -Path ..
+}
